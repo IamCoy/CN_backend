8b98b5553d95d60c8fb74d77ea90934239d6252a
IamCoy <jacoyiaj@yahoo.com>
2022-05-17 00:03:00 -0400

    initial commit

---
+++ sql.sql
@@ -0,0 +1,11 @@
+CREATE TABLE definitions (
+	word_id serial primary key,
+	word text,
+	meaning text
+	
+);
+
+INSERT into definitions (word, meaning) values ('pressure', 'a person with great potential and/or desirable characteristics');
+ INSERT into definitions (word, meaning) values ('top tier', 'a person with highly desirable characteristics');
+
+
